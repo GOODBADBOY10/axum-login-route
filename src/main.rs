d1173196a43491ed8bc08b8387155c9d9a27acae
Goodbadboy <selfmanademola@gmail.com>
2025-10-08 23:51:15 +0100

    feat: implemented my first api login route

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 pub use self::error::{Error, Result};
 
+use axum::middleware;
+use axum::response::Response;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 use std::net::SocketAddr;
 use axum::extract::Query;
@@ -22,6 +25,8 @@ async fn main() {
     let routes_all = Router::new()
     .merge(routes_hello())
     .merge(web::routes_login::routes())
+    .layer(middleware::map_response(main_response_mapper))
+    .layer(CookieManagerLayer::new())
     .fallback_service(routes_static());
 
 
@@ -38,6 +43,13 @@ fn routes_hello() -> Router {
     Router::new()
     .route("/hello", get(handler_hello))
     .route("/hello2", get(handler_hello))
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("-->> {:<12} - main_response_mapper", "MAPPER");
+
+    println!();
+    res
 }
 
 
--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,8 +1,14 @@
-use crate::{Error, Result};
+use crate::{web, Error, Result};
 use axum::{routing::post, Json, Router};
 use serde::Deserialize;
 use serde_json::{json, Value};
+use tower_cookies::{Cookie, Cookies};
 
+#[derive(Debug, Deserialize)]
+struct LoginPayload {
+    username: String,
+    pwd: String,
+}
 
 pub fn routes() -> Router {
     Router::new()
@@ -10,17 +16,15 @@ pub fn routes() -> Router {
     post(api_login))
 }
 
-
-async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>> {
     println!("-->> {:<12} - api_login - {payload:?}", "HANDLER");
-
     //TODO: Implement actual login logic here
     if payload.username != "demo1" || payload.pwd != "welcome" {
         return Err(Error::LoginFail);
     }
-
+    // FIXME: Implement reat auth-token generation/signature
+    cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
     //TODO: Set Cookies or JWT Token
-
     // Create the success body
     let body = Json(json!({
         "result": {
@@ -31,8 +35,8 @@ async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
     Ok(body)
 }
 
-#[derive(Debug, Deserialize)]
-struct LoginPayload {
-    username: String,
-    pwd: String,
-}
+// #[derive(Debug, Deserialize)]
+// struct LoginPayload {
+//     username: String,
+//     pwd: String,
+// }
